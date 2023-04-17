ae5f0c979ce0f58e95923dda6fb8d72db0ebe3f2
Ruslan <56691165+rand89@users.noreply.github.com>
2023-04-17 09:56:36 +0000

    ver 1

---
+++ randbot-web/src/pages/index.js
@@ -0,0 +1,181 @@
+import { useState } from "react";
+import ReactMarkdown from "react-markdown";
+import Head from "next/head";
+import { createParser } from "eventsource-parser";
+import TextareaAutosize from "react-textarea-autosize";
+
+const SYSTEM_MESSAGE =
+  "You are RandBot, a helpful and verstaile AI created by Ruslan using state-of the art ML models and APIs.";
+
+export default function Home() {
+  const [apiKey, setApiKey] = useState("");
+
+  const [messages, setMessages] = useState([
+    { role: "system", content: SYSTEM_MESSAGE },
+  ]);
+
+  const [userMessage, setUserMessage] = useState("");
+
+  const API_URL = "https://api.openai.com/v1/chat/completions";
+
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      sendRequest();
+    }
+  };
+
+  const sendRequest = async () => {
+    if (!userMessage) {
+      alert("Please enter a message before you hit send");
+    }
+
+    if (!apiKey) {
+      alert(
+        "Please provide your OpenAI API key in the navbar. Get it from https://platform.openai.com . NOTE: Your API key is never sent to our server."
+      );
+      return;
+    }
+
+    const oldUserMessage = userMessage;
+    const oldMessages = messages;
+
+    const updatedMessages = [
+      ...messages,
+      {
+        role: "user",
+        content: userMessage,
+      },
+    ];
+
+    setMessages(updatedMessages);
+    setUserMessage("");
+
+    try {
+      const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify({
+          model: "gpt-3.5-turbo",
+          messages: updatedMessages,
+          stream: true,
+        }),
+      });
+
+      if (response.status !== 200) {
+        throw new Error(
+          `OpenAI API returned an error. Please ensure you've provided the right API key. Check the "Console" or "Network" of your browser's developer tools for details.`
+        );
+      }
+
+      const reader = response.body.getReader();
+
+      let newMessage = "";
+      const parser = createParser((event) => {
+        if (event.type === "event") {
+          const data = event.data;
+          if (data === "[DONE]") {
+            return;
+          }
+          const json = JSON.parse(event.data);
+          const content = json.choices[0].delta.content;
+
+          if (!content) {
+            return;
+          }
+
+          newMessage += content;
+
+          const updatedMessages2 = [
+            ...updatedMessages,
+            { role: "assistant", content: newMessage },
+          ];
+
+          setMessages(updatedMessages2);
+        } else {
+          return "";
+        }
+      });
+
+      // eslint-disable-next-line
+      while (true) {
+        const { done, value } = await reader.read();
+        if (done) break;
+        const text = new TextDecoder().decode(value);
+        parser.feed(text);
+      }
+    } catch (error) {
+      console.error("error");
+
+      setUserMessage(oldUserMessage);
+      setMessages(oldMessages);
+      window.alert("Error:" + error.message);
+    }
+  };
+
+  return (
+    <>
+      <Head>
+        <title>RandBot</title>
+      </Head>
+      <div className="flex flex-col h-screen">
+        {/* Navigation Bar */}
+        <nav className="shadow px-4 py-2 flex flex-row justify-between items-center">
+          <div className="text-xl font-bold">RandBot</div>
+          <div>
+            <input
+              type="password"
+              className="border p-1 rounded"
+              onChange={(e) => setApiKey(e.target.value)}
+              value={apiKey}
+              placeholder="API Key"
+            />
+          </div>
+        </nav>
+
+        {/* Message History */}
+        <div className="flex-1 overflow-y-scroll ">
+          <div className="w-full max-w-screen-md mx-auto px-4">
+            {messages
+              .filter((message) => message.role !== "system")
+              .map((message, idx) => (
+                <div key={idx} className="my-3">
+                  <div className="font-bold">
+                    {message.role === "user" ? "You" : "RandBot"}
+                  </div>
+                  <div className="text-lg prose">
+                    <ReactMarkdown>{message.content}</ReactMarkdown>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </div>
+
+        {/* Message Input Box */}
+        <div>
+          <div className="w-full max-w-screen-md mx-auto flex px-4 pb-4 items-start">
+            <TextareaAutosize
+              value={userMessage}
+              autoFocus
+              maxRows={10}
+              onKeyDown={handleKeyDown}
+              placeholder="Ask me anything.."
+              onChange={(e) => setUserMessage(e.target.value)}
+              className="border text-lg rounded-md p-2 flex-1 resize-none"
+              rows={1}
+            />
+            <button
+              onClick={sendRequest}
+              className="bg-blue-500 hover:bg-blue-600 border rounded-md text-white text-lg w-20 p-2 ml-2"
+            >
+              Send
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
